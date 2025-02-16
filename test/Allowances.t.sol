6313a8938291cf427c218e060ed09516258f6c47
Ryan Quinn <ryan@rquinn.org>
2025-02-16 03:30:55 -0600

    Adds docstrings/comments

--- test/Allowances.t.sol
+++ test/Allowances.t.sol
@@ -10,7 +10,6 @@ import {ERC20Impl} from "../src/ERC20Impl.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Approve is Test {
-
     IERC20 internal ierc20;
     ERC20Impl internal token;
     address internal owner;
@@ -19,21 +18,26 @@ contract Approve is Test {
     uint256 internal spenderAllowance = 50 ether;
 
     function setUp() public {
-        spender = vm.addr(1); 
-        owner = vm.addr(2); 
+        spender = vm.addr(1);
+        owner = vm.addr(2);
 
         vm.startPrank(owner);
-        token = new ERC20Impl(ownerInitBal);  
+        token = new ERC20Impl(ownerInitBal);
         vm.stopPrank();
 
         ierc20 = IERC20(address(token));
     }
 
+    /**
+     * @dev Tests the `approve` function of the ERC20 contract.
+     * @param _amount The amount of tokens the owner is allowing the spender to use. Assumed less than the owners balance.
+     */
     function test_Approval(uint256 _amount) public {
         vm.assume(_amount < ownerInitBal);
 
         vm.startPrank(owner);
         vm.expectEmit(true, true, false, true, address(ierc20));
+        // We expect an Approval event.
         emit IERC20.Approval(address(owner), address(spender), _amount);
         bool approved = ierc20.approve(spender, _amount);
         vm.stopPrank();
@@ -41,11 +45,9 @@ contract Approve is Test {
         vm.assertEq(approved, true, "Approve returned false");
         vm.assertEq(ierc20.allowance(owner, spender), _amount, "Allowance not correct");
     }
-
 }
 
 contract Allowance is Test {
-
     IERC20 internal ierc20;
     ERC20Impl internal token;
     address internal owner;
@@ -54,17 +56,21 @@ contract Allowance is Test {
     uint256 internal spenderAllowance = 50 ether;
 
     function setUp() public {
-        spender = vm.addr(1); 
-        owner = vm.addr(2); 
+        spender = vm.addr(1);
+        owner = vm.addr(2);
 
         vm.startPrank(owner);
-        token = new ERC20Impl(ownerInitBal);  
+        token = new ERC20Impl(ownerInitBal);
         vm.stopPrank();
 
         ierc20 = IERC20(address(token));
     }
 
-   function test_Allownace(uint256 _amount) public {
+    /**
+     * @dev Tests retrieval of allowance after approval.
+     * @param _amount The amount approved for the spender. Assumed to be within the owner's initial balance.
+     */
+    function test_Allownace(uint256 _amount) public {
         vm.assume(_amount < ownerInitBal);
 
         vm.startPrank(owner);
@@ -77,7 +83,12 @@ contract Allowance is Test {
         vm.assertEq(ierc20.allowance(owner, spender), _amount, "Allowance not correct");
     }
 
-   function test_ZeroAllownace(address _checkAddress) public {
+    /**
+     * @dev Tests setting and checking for a zero allowance.
+     * Checks that allowance is correctly set to 0, regardless of the address being checked against.
+     * @param _checkAddress Any address. The allowance should be 0 regardless.
+     */
+    function test_ZeroAllownace(address _checkAddress) public {
         vm.startPrank(owner);
         vm.expectEmit(true, true, false, true, address(ierc20));
         emit IERC20.Approval(address(owner), address(spender), 0);
@@ -87,5 +98,4 @@ contract Allowance is Test {
         vm.assertEq(approved, true, "Approve returned false");
         vm.assertEq(ierc20.allowance(owner, _checkAddress), 0, "Allowance not correct");
     }
-
 }
--- test/Balances.t.sol
+++ test/Balances.t.sol
@@ -10,25 +10,28 @@ import {ERC20Impl} from "../src/ERC20Impl.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract BalanceOf is Test {
-
     IERC20 internal ierc20;
     ERC20Impl internal token;
     address internal owner;
     address internal spender;
+    // TODO: These really shouldn't be hardcoded>...
     uint256 internal ownerInitBal = 100 ether;
     uint256 internal spenderAllowance = 50 ether;
 
     function setUp() public {
-        spender = vm.addr(1); 
-        owner = vm.addr(2); 
+        spender = vm.addr(1);
+        owner = vm.addr(2);
 
         vm.startPrank(owner);
-        token = new ERC20Impl(ownerInitBal);  
+        token = new ERC20Impl(ownerInitBal);
         vm.stopPrank();
 
         ierc20 = IERC20(address(token));
     }
 
+    /**
+     * @dev Tests the `balanceOf` function to ensure it correctly returns the owner's initial balance.
+     */
     function test_balanceOf() public {
         vm.startPrank(owner);
         uint256 balance = ierc20.balanceOf(owner);
@@ -36,11 +39,9 @@ contract BalanceOf is Test {
 
         vm.assertEq(balance, ownerInitBal, "Balance not correct");
     }
-
-} 
+}
 
 contract TotalSupply is Test {
-
     IERC20 internal ierc20;
     ERC20Impl internal token;
     address internal owner;
@@ -49,19 +50,36 @@ contract TotalSupply is Test {
     uint256 internal spenderAllowance = 50 ether;
 
     function setUp() public {
-        spender = vm.addr(1); 
-        owner = vm.addr(2); 
+        spender = vm.addr(1);
+        owner = vm.addr(2);
 
         vm.startPrank(owner);
-        token = new ERC20Impl(ownerInitBal);  
+        token = new ERC20Impl(ownerInitBal);
         vm.stopPrank();
 
         ierc20 = IERC20(address(token));
     }
 
-
+    /**
+     * @dev Tests the `totalSupply` function. Total supply after no operations should be the initial supply.
+     */
     function test_TotalSupply() public {
         vm.startPrank(owner);
+        uint256 totalSupply = ierc20.totalSupply();
+        vm.stopPrank();
+
+        vm.assertEq(totalSupply, ownerInitBal, "Total supply not correct");
+    }
+
+    /**
+     * @dev Tests the `totalSupply` function after a transfer. Total supply should remain constant.
+     * @param amount The amount of tokens to transfer. Assumed to be less than or equal to the owner's balance.
+     */
+    function test_AfterTransfer(uint256 amount) public {
+        vm.assume(amount <= ownerInitBal);
+
+        vm.startPrank(owner);
+        ierc20.transfer(spender, amount);
         uint256 totalSupply = ierc20.totalSupply();
         vm.stopPrank();
 
